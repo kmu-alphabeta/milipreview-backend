8f7bcb7cc0121c38df4f0ba087d9cf75dcdc1b15
cg10036 <cg10036@gmail.com>
2024-10-16 20:05:27 +0900

    hotfix: fix register dto

--- src/auth/dtos/register.dto.ts
+++ src/auth/dtos/register.dto.ts
@@ -26,7 +26,7 @@ export class RegisterBodyDto {
 
   @IsNotEmpty()
   @IsString()
-  @IsDate()
+  @Matches(/^20\d{2}\-(0[1-9]|1[0-2])\-(0[1-9]|[1-2][0-9]|3[0-1])$/) // 20XX-XX-XX
   birth: string;
 
   @IsNotEmpty()
