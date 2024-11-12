a5b89cd8f8c42d8bae1c02a883d88531ddf8192a
sinam7 <wjdtjddnjs7@gmail.com>
2024-11-12 11:19:30 +0900

    docs(swagger): Swagger 추가

--- src/ping/ping.controller.ts
+++ src/ping/ping.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { PingService } from './ping.service';
+import { ApiExcludeController } from "@nestjs/swagger";
 
+@ApiExcludeController()
 @Controller('ping')
 export class PingController {
   constructor(private readonly pingService: PingService) {}
