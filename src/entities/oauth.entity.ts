8eb8bc8d8fb7d4ec7b6e6d7c417cb4485db59532
cg10036 <cg10036@gmail.com>
2024-11-19 16:18:57 +0900

    fix swagger

--- src/entities/oauth.entity.ts
+++ src/entities/oauth.entity.ts
@@ -6,22 +6,32 @@ import {
   Property,
 } from '@mikro-orm/core';
 import { User } from './user.entity';
+import { Exclude, instanceToPlain } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Oauth {
+  toJSON() {
+    return instanceToPlain(this);
+  }
+
+  @ApiProperty()
   @PrimaryKey({
     type: new BigIntType('bigint'),
   })
   id!: bigint;
 
+  @Exclude({ toPlainOnly: true })
   @OneToOne(() => User)
   user: User;
 
+  @ApiProperty()
   @Property({
     type: 'text',
   })
   type!: string;
 
+  @ApiProperty()
   @Property({
     type: 'text',
   })
