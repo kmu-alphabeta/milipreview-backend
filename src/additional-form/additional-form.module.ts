dbacc8a69f334935dcc9f7444c487c31680fa090
sinam7 <muwon777@naver.com>
2024-11-23 10:37:00 +0900

    fix(dependency-form,prediction): 내부 모듈 간 export-import 누락 수정

--- src/additional-form/additional-form.module.ts
+++ src/additional-form/additional-form.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdditionalFormService } from './additional-form.service';
 import { AdditionalFormController } from './additional-form.controller';
+import { PredictionModule } from '../prediction/prediction.module';
 
 @Module({
   controllers: [AdditionalFormController],
   providers: [AdditionalFormService],
+  imports: [PredictionModule],
 })
 export class AdditionalFormModule {}
